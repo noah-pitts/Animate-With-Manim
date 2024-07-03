895c8ba029bd4c173256994b2197f1d9bbc7badd
Noah Pitts <noah.pitts713@gmail.com>
2024-07-03 15:07:38 -0500

    intro to shapes

--- intro.py
+++ intro.py
@@ -1,8 +1,18 @@
 from manim import *
 
 
-class CreateCircle(Scene):
+class CreateShapes(Scene):
     def construct(self):
+
+        triangle = Triangle()  # create a triangle
+        triangle.set_fill(BLUE, opacity=1)  # set the color and transparency
+        self.play(Create(triangle))  # show the triangle on screen
+
         circle = Circle()  # create a circle
-        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+        circle.set_fill(RED, opacity=1)  # set the color and transparency
         self.play(Create(circle))  # show the circle on screen
+
+        square = Square()  # create a square
+        square.set_fill(GREEN, opacity=1)  # set the color and transparency
+        self.play(Create(square))  # show the square on screen
+
