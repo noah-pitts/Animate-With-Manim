4698a9bde4f2af5c3e793f6c7925842d24453091
Noah Pitts <noah.pitts713@gmail.com>
2024-07-03 14:58:52 -0500

    create text init

--- create_text.py
+++ create_text.py
@@ -2,7 +2,9 @@ from manim import *
 
 class hello(Scene):
     def construct(self):
-        en_hello = Text("Hello")
-        esp_hello = Text("Hola")
-        self.play(Write(en_hello))
+        en_hello = Text("Hello!")
+        esp_hello = Text("Hola!")
+        self.play(Create(en_hello), run_time=2)
+        self.wait(1)
+        self.play(ReplacementTransform(en_hello,esp_hello))
         self.wait(3)
